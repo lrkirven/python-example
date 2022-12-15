4c881b5ffbe8db6f3a2bccb0a94335606cd73ea6
Nathaniel Varkiani <varkiani.nathaniel@gmail.com>
2022-12-14 20:26:33 -0600

    Adding example code

---
+++ calculator.py
@@ -0,0 +1,13 @@
+import sys
+
+print('Argument List:', str(sys.argv))
+# 0, 1, 2, 3
+# ['calculator.py', 'multiply', '12', '3']
+operator = str(sys.argv[1])
+val1 = int(sys.argv[2])
+val2 = int(sys.argv[3])
+print(operator)
+print(val1)
+print(val2)
+var3 = 4 * 3
+var3 = 4 + 3
